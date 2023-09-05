99fb60d9ac8585a3f445e58ee29be70e857188ef
Niklas Schmolenski <0412niklas@gmail.com>
2023-09-05 09:36:06 +0200

    Implemented variations

--- ican.py
+++ ican.py
@@ -1,47 +1,63 @@
-import numpy as np
-from scipy.optimize import minimize
-from sklearn.manifold import Isomap
+from sklearn.manifold import Isomap, TSNE, LocallyLinearEmbedding, SpectralEmbedding
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel, Matern, DotProduct, ExpSineSquared, RationalQuadratic
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, RBF, ExpSineSquared
-from sklearn.neighbors import NearestNeighbors
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import mutual_info_regression
+from scipy.optimize import minimize
 from hsic import hsic_gam
+import numpy as np
 
-# Initial dimensionality Reduction using Isomap
-def dimReduction(X, Y):
+
+# Initial dimensionality Reduction
+def dimReduction(X, Y, method, neighbor_percentage):
     X = X.flatten()
     Y = Y.flatten()
-    data = np.concatenate((X[:, np.newaxis], Y[:, np.newaxis]), axis=1)  # Isomap requires 2 columns (samples x dimensions)
-
-    neighbors = int(X.shape[0] * 0.1)  # high value: more neighbors are taken into account => smoother curve (less complexity)
-
-    iso = Isomap(n_components=1, n_neighbors=neighbors)  # n_components = 1 because T_hat is 1-dimensional
-    iso.fit(data)
-    T_hat = iso.transform(data)
-
+    data = np.concatenate((X[:, np.newaxis], Y[:, np.newaxis]), axis=1)
+    neighbors = max(int(X.shape[0] * neighbor_percentage), 2)
+    
+    if method == "Isomap":
+    	iso = Isomap(n_components=1, n_neighbors=neighbors)
+    	T_hat = iso.fit_transform(data)
+    elif method == "PCA":
+        pca = PCA(n_components=1)
+        T_hat = pca.fit_transform(data)
+    elif method == "TSNE":
+        tsne = TSNE(n_components=1)
+        T_hat = tsne.fit_transform(data)
+    elif method == "KernelPCA":
+        kpca = KernelPCA(n_components=1)
+        T_hat = kpca.fit_transform(data)
+    elif method == "LLE":
+        lle = LocallyLinearEmbedding(n_components=1, n_neighbors=neighbors)
+        T_hat = lle.fit_transform(data)
+    elif method == "SpectralEmbedding":
+        se = SpectralEmbedding(n_components=1, n_neighbors=neighbors)
+        T_hat = se.fit_transform(data)
+        
     return T_hat
 
-def fitCurve(X, Y):
-    # Initial dimensionality reduction
-    T_hat = dimReduction(X, Y)
-    
-    # Define kernel for Gaussian Process Regression
-    kernel = RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2)) + WhiteKernel(noise_level=1.0, noise_level_bounds=(1e-2, 0.5))
-    s1_hat = GaussianProcessRegressor(kernel=kernel, alpha=1e-1, normalize_y=True, n_restarts_optimizer=10)
-    s2_hat = GaussianProcessRegressor(kernel=kernel, alpha=1e-1, normalize_y=True, n_restarts_optimizer=10)
 
-    for _ in range(5):
+def fitCurve(X, Y, dim_reduction, neighbor_percentage, regression_method, kernel, min_distance, iterations=5):
+    T_hat = dimReduction(X, Y, dim_reduction, neighbor_percentage)
+    
+    s1_hat, s2_hat = regression(T_hat, X, Y, 0.0, 0.0, regression_method, kernel)
+
+    for _ in range(iterations):
         # Step 1: Estimate s_hat using Gaussian Process Regression
         s1_hat.fit(T_hat, X)
         s2_hat.fit(T_hat, Y)
 
-        # Step 2: Update T_hat so that l2-distance is minimized
+        # Step 2: Update T_hat such that l2-distance is minimized
         def l2dist(T):
             T = T.reshape(-1, 1)
             return np.linalg.norm([s1_hat.predict(T).reshape(-1, 1) - X, s2_hat.predict(T).reshape(-1, 1) - Y])
 
         # Minimize l2-distance w.r.t. T_hat
         init_guess = T_hat.flatten()
-        T_hat_new = minimize(l2dist, init_guess, method="L-BFGS-B").x.reshape(-1, 1)
+        T_hat_new = minimize(l2dist, init_guess, method=min_distance).x.reshape(-1, 1)
         
         # Check for convergence
         if np.linalg.norm(T_hat_new - T_hat) / np.linalg.norm(T_hat) < 1e-4:
@@ -51,108 +67,171 @@ def fitCurve(X, Y):
 
     return s1_hat, s2_hat, T_hat
 
-# Independence test using Hilbert-Schmidt Independence Criterion
-def dep(X1, X2):
+
+# Independence test
+def compute_test_statistic(X1, X2, method, threshold):
+    if method == "HSIC":
+    	return hsic_gam(X1, X2, alph=threshold)
+    elif method == "MI":
+    	return mutual_info_regression(X1, np.squeeze(X2))
+
+
+def dep(X1, X2, method, threshold):
     X1 = X1.reshape(-1, 1)
     X2 = X2.reshape(-1, 1)
-
-    testStat, thresh, p = hsic_gam(X1, X2, alph=0.05)
     
-    return testStat < thresh  # is true if X1, X2 are independent
+    test_statistic = compute_test_statistic(X1, X2, method, threshold)
+    
+    if method == "HSIC":
+    	testStat, thresh, p = test_statistic
+    	return testStat < thresh	# is true if X1, X2 are independent
+    elif method == "MI":
+    	mi = test_statistic
+    	return mi < threshold
 
-def areIndependent(T_hat, Nx_hat, Ny_hat):
-    return dep(Nx_hat, Ny_hat) and dep(Nx_hat, T_hat) and dep(Ny_hat, T_hat)
 
-def projection(T_hat, s1_hat, s2_hat, X, Y):
+def areIndependent(T_hat, Nx_hat, Ny_hat, method, threshold):
+    return dep(Nx_hat, Ny_hat, method, threshold) and dep(Nx_hat, T_hat, method, threshold) and dep(Ny_hat, T_hat, method, threshold)
+
+
+def projection(T_hat, s1_hat, s2_hat, X, Y, method, threshold, min_projection):
     def depSum(T):
         T = T.reshape(-1, 1)
 
         Nx_hat = X - s1_hat.predict(T).reshape(-1, 1)
         Ny_hat = Y - s2_hat.predict(T).reshape(-1, 1)
 
-        testStat1, thresh1, p1 = hsic_gam(Nx_hat, Ny_hat, alph=0.05)
-        testStat2, thresh2, p2 = hsic_gam(Nx_hat, T, alph=0.05)
-        testStat3, thresh3, p3 = hsic_gam(Ny_hat, T, alph=0.05)
-
-        score1 = testStat1 - thresh1
-        score2 = testStat2 - thresh2
-        score3 = testStat3 - thresh3
-
-        return score1 + score2 + score3
+        if method == "HSIC":
+        	_, _, p1 = compute_test_statistic(Nx_hat, Ny_hat, method, threshold)
+        	_, _, p2 = compute_test_statistic(Nx_hat, T, method, threshold)
+        	_, _, p3 = compute_test_statistic(Ny_hat, T, method, threshold)
+        	
+        	return -(p1 + p2 + p3)
+        elif method == "MI":
+        	mi1 = compute_test_statistic(Nx_hat, np.squeeze(Ny_hat), method, threshold)
+        	mi2 = compute_test_statistic(Nx_hat, np.squeeze(T), method, threshold)
+        	mi3 = compute_test_statistic(Ny_hat, np.squeeze(T), method, threshold)
+        	
+        	return mi1 + mi2 + mi3
 
     # Minimize dependence w.r.t. T_hat
     init_guess = T_hat.flatten()
-    T_hat = minimize(depSum, init_guess, method="Nelder-Mead").x.reshape(-1, 1)
+    T_hat = minimize(depSum, init_guess, method=min_projection).x.reshape(-1, 1)
 
     return T_hat
 
-# Regression using Gaussian Processes
-def regressionGPR(T_hat, X, Y, Nx_hat, Ny_hat):
-    kernel = RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2)) + WhiteKernel(noise_level=1.0, noise_level_bounds=(1e-2, 0.5))
-    
-    s1_hat = GaussianProcessRegressor(kernel=kernel, alpha=1e-1, normalize_y=True, n_restarts_optimizer=10)
-    s2_hat = GaussianProcessRegressor(kernel=kernel, alpha=1e-1, normalize_y=True, n_restarts_optimizer=10)
 
+def getKernel(kernel):
+    if kernel == "Matern + White":
+        kernel = Matern(length_scale_bounds="fixed") + WhiteKernel()
+    elif kernel == "Matern + ExpSineSquared":
+        kernel = Matern(length_scale_bounds="fixed") + ExpSineSquared(length_scale_bounds="fixed")
+    elif kernel == "Matern":
+        kernel = Matern(length_scale_bounds="fixed")
+    elif kernel == "RBF + White":
+        kernel = RBF(length_scale_bounds="fixed") + WhiteKernel()
+    elif kernel == "RBF":
+        kernel = RBF(length_scale_bounds="fixed")
+    elif kernel == "DotProduct + White":
+        kernel = DotProduct() + WhiteKernel()
+    elif kernel == "DotProduct":
+        kernel = DotProduct()
+    elif kernel == "RationalQuadratic + White":
+        kernel = RationalQuadratic() + WhiteKernel()
+    elif kernel == "RationalQuadratic":
+        kernel = RationalQuadratic()
+    elif kernel == "ExpSineSquared + White":
+        kernel = ExpSineSquared(length_scale_bounds="fixed") + WhiteKernel()
+    elif kernel == "ExpSineSquared":
+        kernel = ExpSineSquared(length_scale_bounds="fixed")
+    
+    return kernel
+
+
+def regression(T_hat, X, Y, Nx_hat, Ny_hat, method, kernel):
+    kernel = getKernel(kernel)
+    
+    if method == "GPR":
+        s1_hat = GaussianProcessRegressor(kernel=kernel)
+        s2_hat = GaussianProcessRegressor(kernel=kernel)
+    elif method == "DecisionTree":
+        s1_hat = DecisionTreeRegressor()
+        s2_hat = DecisionTreeRegressor()
+    elif method == "RandomForest":
+        s1_hat = RandomForestRegressor()
+        s2_hat = RandomForestRegressor()
+    
     s1_hat.fit(T_hat, X - Nx_hat)
     s2_hat.fit(T_hat, Y - Ny_hat)
-
+    
     return s1_hat, s2_hat
 
-# ICAN algorithm
-def identify_confounders(X, Y, K=4):
-    s1_hat, s2_hat, T_hat = fitCurve(X, Y)
 
-    for _ in range(K):
-        T_hat = projection(T_hat, s1_hat, s2_hat, X, Y)
+# ICAN algorithm
+def identify_confounders(X, Y, dim_reduction, neighbor_percentage, iterations, kernel, independence_threshold, regression_method, independence_method, min_distance, min_projection):
+    s1_hat, s2_hat, T_hat = fitCurve(X, Y, dim_reduction, neighbor_percentage, regression_method, kernel, min_distance)
+    
+    for _ in range(iterations):	
+        T_hat = projection(T_hat, s1_hat, s2_hat, X, Y, independence_method, independence_threshold, min_projection)
 
         # Compute residuals
         Nx_hat = X - s1_hat.predict(T_hat).reshape(-1, 1)
         Ny_hat = Y - s2_hat.predict(T_hat).reshape(-1, 1)
 
-        if areIndependent(T_hat, Nx_hat, Ny_hat):
-            return [T_hat, s1_hat, s2_hat, np.var(Nx_hat) / np.var(Ny_hat), True]
+        if areIndependent(T_hat, Nx_hat, Ny_hat, independence_method, independence_threshold):
+            return [T_hat, s1_hat, s2_hat, np.round(np.var(Nx_hat) / np.var(Ny_hat), 4), True]
 
-        s1_hat, s2_hat = regressionGPR(T_hat, X, Y, Nx_hat, Ny_hat)
+        s1_hat, s2_hat = regression(T_hat, X, Y, Nx_hat, Ny_hat, regression_method, kernel)
+    
+    # Compute residuals
+    Nx_hat = X - s1_hat.predict(T_hat).reshape(-1, 1)
+    Ny_hat = Y - s2_hat.predict(T_hat).reshape(-1, 1)
+    
+    return [T_hat, s1_hat, s2_hat, np.round(np.var(Nx_hat) / np.var(Ny_hat), 4), False]  # no CAN-model fitted
 
-    return [T_hat, s1_hat, s2_hat, -1, False]  # no CAN-model fitted
 
 # Check if X->Y can be rejected
-def check_model(X, Y):
-    # Gaussian Process Regression of Y onto X
-    kernel = RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2)) + WhiteKernel(noise_level=1.0, noise_level_bounds=(1e-2, 1e2))
-    model = GaussianProcessRegressor(kernel=kernel, alpha=1e-2, normalize_y=True, n_restarts_optimizer=10)
-    
+def check_model(X, Y, kernel, threshold):
+    # Regression of Y onto X
+    kernel = getKernel(kernel)
+    model = GaussianProcessRegressor(kernel=kernel)
     model.fit(X, Y)
+    
     residuals = Y.reshape(-1,1) - model.predict(X).reshape(-1,1)
     
     # Check for dependent residuals using HSIC
-    testStat, thresh, p = hsic_gam(X.reshape(-1,1), residuals.reshape(-1,1), alph=0.05)
-
+    testStat, thresh, p = hsic_gam(X, residuals, alph=threshold)
+    
     if testStat < thresh:
-        return True	# independent residuals
+        return True, np.round(p[0][0], 4)
     else:
-    	return False	# dependent residuals
+    	return False, np.round(p[0][0], 4)	
+
 
 # Distinguish between X->Y, Y->X, X<-T->Y, no CAN model
-def causal_inference(X, Y, threshold=1.5):
+def causal_inference(X, Y, dim_reduction, neighbor_percentage, iterations, kernel, variance_threshold, independence_threshold, regression_method, independence_method, min_distance, min_projection):
+    # Normalize to variance 1
+    X = (X - np.mean(X)) / np.std(X)
+    Y = (Y - np.mean(Y)) / np.std(Y)
+    
     # Run ICAN
-    T_hat, s1_hat, s2_hat, var, result = identify_confounders(X, Y)
-    if (result == False):
-    	return T_hat, var, s1_hat, s1_hat, result, "No CAN Model"
+    T_hat, s1_hat, s2_hat, var, result = identify_confounders(X, Y, dim_reduction, neighbor_percentage, iterations, kernel, independence_threshold, regression_method, independence_method, min_distance, min_projection)
    
     # Check if X->Y or Y->X can be rejected
-    modelXY = check_model(X, Y)
-    modelYX = check_model(Y, X)
+    modelXY, p1 = check_model(X, Y, kernel, independence_threshold)
+    modelYX, p2 = check_model(Y, X, kernel, independence_threshold)
+
+    # Check if CAN model was accepted
+    if (result == False):
+    	return T_hat, var, s1_hat, s2_hat, result, "No CAN Model", p1, p2
     
     # Distinguish between X->Y, Y->X and X<-T->Y
     if (not modelXY) and (not modelYX):
-    	return T_hat, var, s1_hat, s2_hat, result, "X<-T->Y"
-    elif modelXY and modelYX:
-    	return T_hat, var, s1_hat, s2_hat, result, "both directions possible"
-    elif modelXY and var < (1/threshold):
-    	return T_hat, var, s1_hat, s2_hat, result, "X->Y"
-    elif modelYX and var > threshold:
-    	return T_hat, var, s1_hat, s2_hat, result, "Y->X"
+    	return T_hat, var, s1_hat, s2_hat, result, "X<-T->Y", p1, p2
+    elif modelXY and var < (1/variance_threshold):
+    	return T_hat, var, s1_hat, s2_hat, result, "X->Y", p1, p2
+    elif modelYX and var > variance_threshold:
+    	return T_hat, var, s1_hat, s2_hat, result, "Y->X", p1, p2
     else:
-    	return T_hat, var, s1_hat, s2_hat, result, "X<-T->Y"
+    	return T_hat, var, s1_hat, s2_hat, result, "X<-T->Y", p1, p2
 
